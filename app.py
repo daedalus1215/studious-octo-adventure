6fc9768620d3ee43d3077c7d6952bb68c08fba06
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-03-06 22:11:18 -0500

    all working, now converting to csv

--- app.py
+++ app.py
@@ -1,38 +1,71 @@
-
-import requests
+import csv
 import datetime
 import sys
 
+import requests
+
 
 class Reward:
-    date = ''
+    unix_timestamp = ''
+    created_date = ''
     value = ''
 
-    def __init__(self, date, value):
-        self.date = date
+    def __init__(self, unix_timestamp, date, value):
+        self.unix_timestamp = unix_timestamp
+        self.created_date = date
         self.value = value
 
-currentValue = sys.argv[1]
-timestamp = sys.argv[2]
-url = sys.argv[3] 
+    def __getitem__(self):
+        return (dict({
+            'unix_timestamp': self.unix_timestamp,
+            'created_date': self.date,
+            'amount': self.value
+        }))
 
-res = requests.get(url).json()
-print(res)
-# dt = datetime.datetime.fromtimestamp(0)
-# print(dt)
-# unix_conversion = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-# print(unix_conversion)
-# timestamp = round(datetime.datetime(2021, 12, 1).timestamp())
+    currentValue = sys.argv[1]
+    timestamp = sys.argv[2]
+    url = sys.argv[3]
 
-print('timestamp:')
-print(timestamp)
+    res = requests.get(url).json()
+    # print(res)
+    # dt = datetime.datetime.fromtimestamp(0)
+    # print(dt)
+    # unix_conversion = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    # print(unix_conversion)
+    # timestamp = round(datetime.datetime(2021, 12, 1).timestamp())
 
-assetFilteredByTimestamp = list(filter(lambda asset: asset[4] > timestamp, res))
+    print('timestamp:')
+    print(timestamp)
 
-check = list(map(lambda x: Reward(x[4], x[11] - currentValue), assetFilteredByTimestamp))
+    runningTallyOfAmount = 0
 
-print(check[0].date, check[0].value)
+    def only_grab_entries_with_changed_amount(asset) -> bool:
+        """
+        We only want to grab entries if the amount changed, because there are a lot of entries that have duplicate amounts
 
+        :param asset: individual rewards
+        :return: true if the rewards is unique
+        """
+        global runningTallyOfAmount
+        if runningTallyOfAmount == float(asset[11]):
+            return False
+        else:
+            runningTallyOfAmount = float(asset[11])
+            return True
 
+    assetFilteredByTimestamp = list(filter(lambda asset: only_grab_entries_with_changed_amount(asset), res))
 
+    check = list(map(lambda x: Reward(str(x[4])[0:-2], datetime.datetime.fromtimestamp(int(str(x[4])[0:-3])),
+                                      float(x[11]) - float(currentValue)),
+                     assetFilteredByTimestamp))
 
+    header_count = 0
+    with open('./studio-octo-adventure.csv', 'w', encoding='UTF8', newline='') as f:
+        # writer = csv.DictWriter(f, fieldnames=fieldnames = ['created_date', 'value'])
+        writer.writeheader()
+        writer.writerows()
+    writer = csv.writer(f)
+    writer.writerows(list(check))
+    # list(forEach(lambda x: writer.writerow(print(str(x.created_date) + '  ' + str(x.value)), check)))
+
+    f.close()
